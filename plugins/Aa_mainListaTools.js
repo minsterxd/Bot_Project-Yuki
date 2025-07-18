816339feadeb849a1930a75db43ac2da3829283e
minsterxd <minsterxd@gmail.com>
2025-07-18 18:12:05 -0300

    Create Aa_mainListaTools.js

---
+++ plugins/Aa_mainListaTools.js
@@ -0,0 +1,68 @@
+let handler = async (m, { conn, args }) => {
+    let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    let user = global.db.data.users[userId]
+    let name = conn.getName(userId)
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
+    
+    let txt = `
+•:･ﾟ⊹˚• \`『 Herramientas 』\` •˚⊹:･ﾟ•
+
+❍ Comandos de herramientas con muchas funciones.
+ᰔᩚ *#calcular • #calcular • #cal*
+> ✦ Calcular todo tipo de ecuaciones.
+ᰔᩚ *#tiempo • #clima*
+> ✦ Ver el clima de un pais.
+ᰔᩚ *#horario*
+> ✦ Ver el horario global de los países.
+ᰔᩚ *#fake • #fakereply*
+> ✦ Crea un mensaje falso de un usuario.
+ᰔᩚ *#enhance • #remini • #hd*
+> ✦ Mejora la calidad de una imagen.
+ᰔᩚ *#letra*
+> ✦ Cambia la fuente de las letras.
+ᰔᩚ *#read • #readviewonce • #ver*
+> ✦ Ver imágenes de una sola vista.
+ᰔᩚ *#whatmusic • #shazam*
+> ✦ Descubre el nombre de canciones o vídeos.
+ᰔᩚ *#ss • #ssweb*
+> ✦ Ver el estado de una página web.
+ᰔᩚ *#length • #tamaño*
+> ✦ Cambia el tamaño de imágenes y vídeos.
+ᰔᩚ *#say • #decir* + [texto]
+> ✦ Repetir un mensaje.
+ᰔᩚ *#todoc • #toducument*
+> ✦ Crea documentos de (audio, imágenes y vídeos).
+ᰔᩚ *#translate • #traducir • #trad*
+> ✦ Traduce palabras en otros idiomas.
+  `.trim()
+
+  await conn.sendMessage(m.chat, { 
+      text: txt,
+      contextInfo: {
+          mentionedJid: [m.sender, userId],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+      },
+  }, { quoted: m })
+
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['ListaTools']
+
+export default handler
+
+function clockString(ms) {
+    let seconds = Math.floor((ms / 1000) % 60)
+    let minutes = Math.floor((ms / (1000 * 60)) % 60)
+    let hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+    return `${hours}h ${minutes}m ${seconds}s`
+}
