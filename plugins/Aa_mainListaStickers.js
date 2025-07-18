87c344f5d7e27a7ea69087eb2c74509b3b9febc7
minsterxd <minsterxd@gmail.com>
2025-07-18 18:10:54 -0300

    Create Aa_mainListaStickers.js

---
+++ plugins/Aa_mainListaStickers.js
@@ -0,0 +1,60 @@
+let handler = async (m, { conn, args }) => {
+    let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    let user = global.db.data.users[userId]
+    let name = conn.getName(userId)
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
+    
+    let txt = `
+• :･ﾟ⊹˚• \`『 Stickers 』\` •˚⊹:･ﾟ•
+
+❍ Comandos para creaciones de stickers etc.
+ᰔᩚ *#sticker • #s*
+> ✦ Crea stickers de (imagen/video)
+ᰔᩚ *#setmeta*
+> ✦ Estable un pack y autor para los stickers.
+ᰔᩚ *#delmeta*
+> ✦ Elimina tu pack de stickers.
+ᰔᩚ *#pfp • #getpic*
+> ✦ Obtén la foto de perfil de un usuario.
+ᰔᩚ *#qc*
+> ✦ Crea stickers con texto o de un usuario.
+ᰔᩚ *#toimg • #img*
+> ✦ Convierte stickers en imagen.
+ᰔᩚ *#brat • #ttp • #attp*︎ 
+> ✦ Crea stickers con texto.
+ᰔᩚ *#emojimix*
+> ✦ Fuciona 2 emojis para crear un sticker.
+ᰔᩚ *#wm*
+> ✦ Cambia el nombre de los stickers.
+  `.trim()
+
+  await conn.sendMessage(m.chat, { 
+      text: txt,
+      contextInfo: {
+          mentionedJid: [m.sender, userId],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+      },
+  }, { quoted: m })
+
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['ListaStickers']
+
+export default handler
+
+function clockString(ms) {
+    let seconds = Math.floor((ms / 1000) % 60)
+    let minutes = Math.floor((ms / (1000 * 60)) % 60)
+    let hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+    return `${hours}h ${minutes}m ${seconds}s`
+}
