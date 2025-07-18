7266570a916a7481ee6db3f8e2877cb5d7a98214
minsterxd <minsterxd@gmail.com>
2025-07-18 18:17:30 -0300

    Create Aa_mainListaNSFW.js

---
+++ plugins/Aa_mainListaNSFW.js
@@ -0,0 +1,84 @@
+let handler = async (m, { conn, args }) => {
+    let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    let user = global.db.data.users[userId]
+    let name = conn.getName(userId)
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
+    
+    let txt = `
+• :･ﾟ⊹˚• \`『 NSFW 』\` •˚⊹:･ﾟ•
+
+❍ Comandos NSFW (Contenido para adultos)
+ᰔᩚ *#anal* + <mencion>
+> ✦ Hacer un anal
+ᰔᩚ *#waifu*
+> ✦ Buscá una waifu aleatorio.
+ᰔᩚ *#bath* + <mencion>
+> ✦ Bañarse
+ᰔᩚ *#blowjob • #mamada • #bj* + <mencion>
+> ✦ Dar una mamada
+ᰔᩚ *#boobjob* + <mencion>
+> ✦ Hacer una rusa
+ᰔᩚ *#cum* + <mencion>
+> ✦ Venirse en alguien.
+ᰔᩚ *#fap* + <mencion>
+> ✦ Hacerse una paja
+ᰔᩚ *#ppcouple • #ppcp*
+> ✦ Genera imagenes para amistades o parejas.
+ᰔᩚ *#footjob* + <mencion>
+> ✦ Hacer una paja con los pies
+ᰔᩚ *#fuck • #coger • #fuck2* + <mencion>
+> ✦ Follarte a alguien
+ᰔᩚ *#cafe • #coffe*
+> ✦ Tomate un cafecito con alguien
+ᰔᩚ *#violar • #perra + <mencion>
+> ✦ Viola a alguien
+ᰔᩚ *#grabboobs* + <mencion>
+> ✦ Agarrrar tetas
+ᰔᩚ *#grop* + <mencion>
+> ✦ Manosear a alguien
+ᰔᩚ *#lickpussy* + <mencion>
+> ✦ Lamer un coño
+ᰔᩚ *#rule34 • #r34* + [Tags]
+> ✦ Buscar imagenes en Rule34
+ᰔᩚ *#sixnine • #69* + <mencion>
+> ✦ Haz un 69 con alguien
+ᰔᩚ *#spank • #nalgada* + <mencion>
+> ✦ Dar una nalgada
+ᰔᩚ *#suckboobs* + <mencion>
+> ✦ Chupar tetas
+ᰔᩚ *#undress • #encuerar* + <mencion>
+> ✦ Desnudar a alguien
+ᰔᩚ *#yuri • #tijeras* + <mencion>
+> ✦ Hacer tijeras.
+  `.trim()
+
+  await conn.sendMessage(m.chat, { 
+      text: txt,
+      contextInfo: {
+          mentionedJid: [m.sender, userId],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+      },
+  }, { quoted: m })
+
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['ListaNsfw']
+
+export default handler
+
+function clockString(ms) {
+    let seconds = Math.floor((ms / 1000) % 60)
+    let minutes = Math.floor((ms / (1000 * 60)) % 60)
+    let hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+    return `${hours}h ${minutes}m ${seconds}s`
+}
