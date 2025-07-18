2d04d2e430d309e4e9524b9d0546fc7a53e95b9b
minsterxd <minsterxd@gmail.com>
2025-07-18 18:47:29 -0300

    Update Aa_mainListaDescargas.js

--- plugins/Aa_mainListaDescargas.js
+++ plugins/Aa_mainListaDescargas.js
@@ -64,9 +64,9 @@ let handler = async (m, { conn, args }) => {
 
 }
 
-handler.help = ['menu']
+handler.help = ['listadescargas']
 handler.tags = ['main']
-handler.command = ['ListaDescargas']
+handler.command = ['listadescargas']
 
 export default handler
 
