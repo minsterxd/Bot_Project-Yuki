d64a98861b013e66ec6633a5b3e681c06332d6a7
minsterxd <minsterxd@gmail.com>
2025-07-18 18:03:37 -0300

    Create Aa_mainListaBuscadores.js

---
+++ plugins/Aa_mainListaBuscadores.js
@@ -0,0 +1,70 @@
+let handler = async (m, { conn, args }) => {
+    let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    let user = global.db.data.users[userId]
+    let name = conn.getName(userId)
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
+    
+    let txt = `
+• :･ﾟ⊹˚• \`『 Buscadores 』\` •˚⊹:･ﾟ•
+
+❍ Comandos para realizar búsquedas en distintas plataformas.
+ᰔᩚ *#tiktoksearch • #tiktoks*
+> ✦ Buscador de videos de tiktok.
+ᰔᩚ *#tweetposts*
+> ✦ Buscador de posts de Twitter/X.
+ᰔᩚ *#ytsearch • #yts*
+> ✦ Realiza búsquedas de Youtube.
+ᰔᩚ *#githubsearch*
+> ✦ Buscador de usuarios de GitHub.
+ᰔᩚ *#cuevana • #cuevanasearch*
+> ✦ Buscador de películas/series por Cuevana.
+ᰔᩚ *#google*
+> ✦ Realiza búsquedas por Google.
+ᰔᩚ *#pin • #pinterest*
+> ✦ Buscador de imagenes de Pinterest.
+ᰔᩚ *#imagen • #image*
+> ✦ buscador de imagenes de Google.
+ᰔᩚ *#infoanime*
+> ✦ Buscador de información de anime/manga.
+ᰔᩚ *#hentaisearch • #searchhentai*
+> ✦ Buscador de capítulos hentai.
+ᰔᩚ #xnxxsearch • #xnxxs*
+> ✦ Buscador de vídeos de Xnxx.
+ᰔᩚ *#xvsearch • #xvideossearch*
+> ✦ Buscador de vídeos de Xvideos.
+ᰔᩚ *#pornhubsearch • #phsearch*
+> ✦ Buscador de videos de Pornhub.
+ᰔᩚ *#npmjs*
+> ✦ Buscandor de npmjs.
+  `.trim()
+
+  await conn.sendMessage(m.chat, { 
+      text: txt,
+      contextInfo: {
+          mentionedJid: [m.sender, userId],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+      },
+  }, { quoted: m })
+
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['ListaBuscadores']
+
+export default handler
+
+function clockString(ms) {
+    let seconds = Math.floor((ms / 1000) % 60)
+    let minutes = Math.floor((ms / (1000 * 60)) % 60)
+    let hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+    return `${hours}h ${minutes}m ${seconds}s`
+}
