e4ca16155863ec65a74b83a589685d2817d99906
minsterxd <minsterxd@gmail.com>
2025-07-18 18:15:20 -0300

    Create Aa_mainListaAnime.js

---
+++ plugins/Aa_mainListaAnime.js
@@ -0,0 +1,110 @@
+let handler = async (m, { conn, args }) => {
+    let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    let user = global.db.data.users[userId]
+    let name = conn.getName(userId)
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
+    
+    let txt = `
+• :･ﾟ⊹˚• \`『 Anime 』\` •˚⊹:･ﾟ•
+
+❍ Comandos de reacciones de anime.
+ᰔᩚ *#angry • #enojado* + <mencion>
+> ✦ Estar enojado
+ᰔᩚ *#bite* + <mencion>
+> ✦ Muerde a alguien
+ᰔᩚ *#bleh* + <mencion>
+> ✦ Sacar la lengua
+ᰔᩚ *#blush* + <mencion>
+> ✦ Sonrojarte
+ᰔᩚ *#bored • #aburrido* + <mencion>
+> ✦ Estar aburrido
+ᰔᩚ *#cry* + <mencion>
+> ✦ Llorar por algo o alguien
+ᰔᩚ *#cuddle* + <mencion>
+> ✦ Acurrucarse
+ᰔᩚ *#dance* + <mencion>
+> ✦ Sacate los pasitos prohíbidos
+ᰔᩚ *#drunk* + <mencion>
+> ✦ Estar borracho
+ᰔᩚ *#eat • #comer* + <mencion>
+> ✦ Comer algo delicioso
+ᰔᩚ *#facepalm* + <mencion>
+> ✦ Darte una palmada en la cara
+ᰔᩚ *#happy • #feliz* + <mencion>
+> ✦ Salta de felicidad
+ᰔᩚ *#hug* + <mencion>
+> ✦ Dar un abrazo
+ᰔᩚ *#impregnate • #preg* + <mencion>
+> ✦ Embarazar a alguien
+ᰔᩚ *#kill* + <mencion>
+> ✦ Toma tu arma y mata a alguien
+ᰔᩚ *#kiss • #besar* • #kiss2 + <mencion>
+> ✦ Dar un beso
+ᰔᩚ *#laugh* + <mencion>
+> ✦ Reírte de algo o alguien
+ᰔᩚ *#lick* + <mencion>
+> ✦ Lamer a alguien
+ᰔᩚ *#love • #amor* + <mencion>
+> ✦ Sentirse enamorado
+ᰔᩚ *#pat* + <mencion>
+> ✦ Acaricia a alguien
+ᰔᩚ *#poke* + <mencion>
+> ✦ Picar a alguien
+ᰔᩚ *#pout* + <mencion>
+> ✦ Hacer pucheros
+ᰔᩚ *#punch* + <mencion>
+> ✦ Dar un puñetazo
+ᰔᩚ *#run* + <mencion>
+> ✦ Correr
+ᰔᩚ *#sad • #triste* + <mencion>
+> ✦ Expresar tristeza
+ᰔᩚ *#scared* + <mencion>
+> ✦ Estar asustado
+ᰔᩚ *#seduce* + <mencion>
+> ✦ Seducir a alguien
+ᰔᩚ *#shy • #timido* + <mencion>
+> ✦ Sentir timidez
+ᰔᩚ *#slap* + <mencion>
+> ✦ Dar una bofetada
+ᰔᩚ *#dias • #days*
+> ✦ Darle los buenos días a alguien 
+ᰔᩚ *#noches • #nights*
+> ✦ Darle las buenas noches a alguien 
+ᰔᩚ *#sleep* + <mencion>
+> ✦ Tumbarte a dormir
+ᰔᩚ *#smoke* + <mencion>
+> ✦ Fumar
+ᰔᩚ *#think* + <mencion>
+> ✦ Pensar en algo
+  `.trim()
+
+  await conn.sendMessage(m.chat, { 
+      text: txt,
+      contextInfo: {
+          mentionedJid: [m.sender, userId],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+      },
+  }, { quoted: m })
+
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['ListaAnime']
+
+export default handler
+
+function clockString(ms) {
+    let seconds = Math.floor((ms / 1000) % 60)
+    let minutes = Math.floor((ms / (1000 * 60)) % 60)
+    let hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+    return `${hours}h ${minutes}m ${seconds}s`
+}
