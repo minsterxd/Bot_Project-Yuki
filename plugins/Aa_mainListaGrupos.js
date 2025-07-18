41c7890579351efb2e0427d92193df3406a2418d
minsterxd <minsterxd@gmail.com>
2025-07-18 18:14:14 -0300

    Create Aa_mainListaGrupos.js

---
+++ plugins/Aa_mainListaGrupos.js
@@ -0,0 +1,104 @@
+let handler = async (m, { conn, args }) => {
+    let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    let user = global.db.data.users[userId]
+    let name = conn.getName(userId)
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
+    
+    let txt = `
+• :･ﾟ⊹˚• \`『 Grupos 』\` •˚⊹:･ﾟ•
+
+❍ Comandos de grupos para una mejor gestión de ellos.
+ᰔᩚ *#hidetag*
+> ✦ Envia un mensaje mencionando a todos los usuarios
+ᰔᩚ *#gp • #infogrupo*
+> ✦  Ver la Informacion del grupo.
+ᰔᩚ *#linea • #listonline*
+> ✦ Ver la lista de los usuarios en linea.
+ᰔᩚ *#setwelcome*
+> ✦ Establecer un mensaje de bienvenida personalizado.
+ᰔᩚ *#setbye*
+> ✦ Establecer un mensaje de despedida personalizado.
+ᰔᩚ *#link*
+> ✦ El bot envia el link del grupo.
+ᰔᩚ *admins • admin*
+> ✦ Mencionar a los admins para solicitar ayuda.
+ᰔᩚ *#restablecer • #revoke*
+> ✦ Restablecer el enlace del grupo.
+ᰔᩚ *#grupo • #group* [open / abrir]
+> ✦ Cambia ajustes del grupo para que todos los usuarios envien mensaje.
+ᰔᩚ *#grupo • #gruop* [close / cerrar]
+> ✦ Cambia ajustes del grupo para que solo los administradores envien mensaje.
+ᰔᩚ *#kick* [número / mension]
+> ✦ Elimina un usuario de un grupo.
+ᰔᩚ *#add • #añadir • #agregar* [número]
+> ✦ Invita a un usuario a tu grupo.
+ᰔᩚ *#promote* [mension / etiquetar]
+> ✦ El bot dara administrador al usuario mencionando.
+ᰔᩚ *#demote* [mension / etiquetar]
+> ✦ El bot quitara administrador al usuario mencionando.
+ᰔᩚ *#gpbanner • #groupimg*
+> ✦ Cambiar la imagen del grupo.
+ᰔᩚ *#gpname • #groupname*
+> ✦ Cambiar el nombre del grupo.
+ᰔᩚ *#gpdesc • #groupdesc*
+> ✦ Cambiar la descripción del grupo.
+ᰔᩚ *#advertir • #warn • #warning*
+> ✦ Darle una advertencia aún usuario.
+ᰔᩚ ︎*#unwarn • #delwarn*
+> ✦ Quitar advertencias.
+ᰔᩚ *#advlist • #listadv*
+> ✦ Ver lista de usuarios advertidos.
+ᰔᩚ *#bot on*
+> ✦ Enciende el bot en un grupo.
+ᰔᩚ *#bot off*
+> ✦ Apaga el bot en un grupo.
+ᰔᩚ *#mute* [mension / etiquetar]
+> ✦ El bot elimina los mensajes del usuario.
+ᰔᩚ *#unmute* [mension / etiquetar]
+> ✦ El bot deja de eliminar los mensajes del usuario.
+ᰔᩚ *#encuesta • #poll*
+> ✦ Crea una encuesta.
+ᰔᩚ *#delete • #del*
+> ✦ Elimina mensaje de otros usuarios.
+ᰔᩚ *#fantasmas*
+> ✦ Ver lista de inactivos del grupo.
+ᰔᩚ *#kickfantasmas*
+> ✦ Elimina a los inactivos del grupo.
+ᰔᩚ *#invocar • #tagall • #todos*
+> ✦ Invoca a todos los usuarios de un grupo.
+ᰔᩚ *#setemoji • #setemo*
+> ✦ Cambia el emoji que se usa en la invitación de usuarios.
+ᰔᩚ *#listnum • #kicknum*
+> ✦ Elimine a usuario por el prefijo de país.
+  `.trim()
+
+  await conn.sendMessage(m.chat, { 
+      text: txt,
+      contextInfo: {
+          mentionedJid: [m.sender, userId],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+      },
+  }, { quoted: m })
+
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['ListaGrupos']
+
+export default handler
+
+function clockString(ms) {
+    let seconds = Math.floor((ms / 1000) % 60)
+    let minutes = Math.floor((ms / (1000 * 60)) % 60)
+    let hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+    return `${hours}h ${minutes}m ${seconds}s`
+}
