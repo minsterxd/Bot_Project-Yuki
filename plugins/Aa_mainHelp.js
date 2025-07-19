1924da5d368ef6c5529337b0a5cb0cac213db00f
minsterxd <minsterxd@gmail.com>
2025-07-19 16:19:41 -0300

    Update Aa_mainHelp.js

--- plugins/Aa_mainHelp.js
+++ plugins/Aa_mainHelp.js
@@ -57,7 +57,7 @@ let img = await fs.readFile("./src/menu.jpg");
       m.chat,
       {
         document: img,
-        fileName: "${botname",
+        fileName: "ᥡᥙkі sᥙ᥆ᥙ",
         mimetype: "image/png",
         caption: txt,
         fileLength: 1900,
