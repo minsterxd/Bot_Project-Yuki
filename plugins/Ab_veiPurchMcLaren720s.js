d82ec3436561533756ae7c5e2165c15bf5cc9218
minsterxd <minsterxd@gmail.com>
2025-07-20 16:17:53 -0300

    Update Ab_veiPurchMcLaren720s.js

--- plugins/Ab_veiPurchMcLaren720s.js
+++ plugins/Ab_veiPurchMcLaren720s.js
@@ -1,7 +1,12 @@
+import { promises as fs } from 'fs';
+
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     if (!user) {
         return conn.reply(m.chat, `${emoji} El usuario no se encuentra en la base de Datos.`, m);
+    }
+    if (user.mclaren720s > 1) {
+        return conn.reply(m.chat, `Ya tienes un McLaren 720s, porque querrías otro?`, m);
     }
     if (user.coin < 10000) {
         return conn.reply(m.chat, `Su dinero es insuficiente para comprar un McLaren720s. 💔`, m);
@@ -9,8 +14,23 @@ let handler = async (m, { conn }) => {
     let PurchMcLaren720s = 100000; 
     user.mclaren720s += PurchMcLaren720s;
     user.coin -= 10000; 
-    let info = `Compraste con éxito tu McLaren 720s. Disfrútalo ♥️. Y gracias por esos deliciosos 10000 Yenes (⁠ ⁠/⁠^⁠ω⁠^⁠)⁠/`;
-    await conn.sendMessage(m.chat, { text: info }, { quoted: m });
+    let img = await fs.readFile("./src/McLaren720s.jpg");
+    let info = `Compraste con éxito tu McLaren 720s. Disfrútalo ❤️. Y gracias por esos deliciosos 10000 Yenes (⁠ ⁠/⁠^⁠ω⁠^⁠)⁠/`;
+    await conn.sendMessage(
+      m.chat,
+      {
+        image: img,
+        fileName: "ᥡᥙkі sᥙ᥆ᥙ",
+        mimetype: "image/png",
+        caption: info,
+        fileLength: 1900,
+        contextInfo: {
+          mentionedJid: mention,
+          isForwarded: true,
+          forwardingScore: 999,
+        },
+      },
+    );
 };
 
 handler.help = ['comprarmclaren'];
