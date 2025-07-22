f2954721e61090a71f6a939ad8cf96693a4d83d3
minsterxd <minsterxd@gmail.com>
2025-07-22 19:41:44 -0300

    Create casas.js

---
+++ src/casas/propiedades/casas.js
@@ -0,0 +1,37 @@
+const casasDisponibles = [
+  {
+    id: 'casa1',
+    tipo: 'Casa pequeña',
+    libre: true,
+    color: 'rojo',
+    capacidad: 5,
+    imagen: './src/casas/Casa_Pequeña_1.jpg'
+  },
+  {
+    id: 'casa2',
+    tipo: 'Casa pequeña',
+    libre: true,
+    color: 'azul',
+    capacidad: 5,
+    imagen: './src/casas/Casa_Pequeña_2.jpg'
+  },
+  {
+    id: 'edificio1',
+    tipo: 'Edificio pequeño',
+    libre: true,
+    color: 'gris',
+    capacidad: 10,
+    imagen: './src/casas/Edificio_Pequeño_1.jpg'
+  },
+  {
+    id: 'mansion1',
+    tipo: 'Mansión',
+    libre: true,
+    color: 'negro',
+    capacidad: 200,
+    imagen: './src/casas/Mansion_1.jpg'
+  }
+  // Puedes agregar más casas aquí...
+];
+
+export default casasDisponibles;
