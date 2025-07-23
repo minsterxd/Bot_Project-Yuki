e07c56d0070bce7ccbd74527c1d4bbc89c050155
minsterxd <minsterxd@gmail.com>
2025-07-22 22:16:30 -0300

    Update casas.js

--- src/propiedades/casas.js
+++ src/propiedades/casas.js
@@ -1,8 +1,7 @@
 const casasDisponibles = [
   {
     id: 'casa1',
-    tipo: 'pequeña', // antes: 'Casa pequeña'
-    libre: true,
+    tipo: 'pequeña',
     color: 'rojo',
     capacidad: 5,
     imagen: './src/casas/Casa_Pequeña_1.jpg'
@@ -10,25 +9,24 @@ const casasDisponibles = [
   {
     id: 'casa2',
     tipo: 'pequeña',
-    libre: true,
     color: 'azul',
     capacidad: 5,
     imagen: './src/casas/Casa_Pequeña_2.jpg'
   },
   {
     id: 'edificio1',
-    tipo: 'edificiopequeño', // antes: 'Edificio pequeño'
-    libre: true,
+    tipo: 'edificiopequeño',
     color: 'gris',
     capacidad: 10,
     imagen: './src/casas/Edificio_Pequeño_1.jpg'
   },
   {
     id: 'mansion1',
-    tipo: 'mansion', // antes: 'Mansión'
-    libre: true,
+    tipo: 'mansion',
     color: 'negro',
     capacidad: 200,
     imagen: './src/casas/Mansion_1.jpg'
   }
 ];
+
+export default casasDisponibles;
